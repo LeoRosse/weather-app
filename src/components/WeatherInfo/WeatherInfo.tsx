55ebdb767e0423a00ce7f9c437cf42fb7fd39dd2
leorosse <leorosse87@gmail.com>
2020-01-19 14:42:51 +0100

    Editing Weather component

--- src/components/WeatherInfo/WeatherInfo.tsx
+++ src/components/WeatherInfo/WeatherInfo.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Weather } from "../../@typings/weather";
 import { Image } from "../../@typings/image";
-
+import "./WeatherInfo.styles.scss";
+import { convertTimeStampToDate } from "./utils";
 interface WeatherInfoProps {
   showInfo: boolean;
   weather: Weather;
@@ -15,26 +16,43 @@ export const WeatherInfo: React.FC<WeatherInfoProps> = ({
 }) =>
   showInfo ? (
     <div className="card">
-      <img src={image.urls.small} alt="City" />
+      <div className="photo__container">
+        <img src={image.urls.small} alt="City" className="photo" />
+      </div>
       <div className="title-content">
         <h3>{weather.name}</h3>
         <hr />
-        <div className="intro">
-          Yllamco laboris nisi ut aliquip ex ea commodo.
-        </div>
+        <div className="intro">{image.location.title}</div>
       </div>
-      <p>
-        {weather.weather[0].description}
-        <br />
-        temperature: {Math.round(weather.main.temp)}°C
-        <br />
-        perceived temperature: {Math.round(weather.main.feels_like)}°C
-        <br />
-        humidity: {Math.round(weather.main.humidity)}%
-        <br />
-        temperature max: {Math.round(weather.main.temp_max)}°C
-        <br />
-        temperature min: {Math.round(weather.main.temp_min)}°C
+      <p className="info">
+        <label>{weather.weather[0].description}</label>
+        <span>
+          <label>temperature:</label> {Math.round(weather.main.temp)}°C
+        </span>
+        <span>
+          <label>perceived temperature:</label>{" "}
+          {Math.round(weather.main.feels_like)}°C
+        </span>
+        <span>
+          <label htmlFor="">humidity:</label>{" "}
+          {Math.round(weather.main.humidity)}%
+        </span>
+        <span>
+          <label htmlFor="">temperature max:</label>{" "}
+          {Math.round(weather.main.temp_max)}°C
+        </span>
+        <span>
+          <label htmlFor="">temperature min:</label>{" "}
+          {Math.round(weather.main.temp_min)}°C
+        </span>
+        <span>
+          <label htmlFor="">sunrise: </label>{" "}
+          {convertTimeStampToDate(weather.sys.sunrise)}
+        </span>
+        <span>
+          <label htmlFor="">sunset: </label>{" "}
+          {convertTimeStampToDate(weather.sys.sunset)}
+        </span>
       </p>
     </div>
   ) : null;
---
+++ src/components/WeatherInfo/utils.ts
@@ -0,0 +1,6 @@
+export const convertTimeStampToDate = (timestamp: number) => {
+    const date = new Date(timestamp * 1000);
+    const hour = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    return hour + ":" + minutes.substr(-2);
+}
